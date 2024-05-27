8b7996ce13295cd2f18ef1a36adb3cea819715ec
AngeloRakotobe <angeloRakotobe@outlook.fr>
2024-05-27 03:16:39 +0300

    Ajout de spinner lors de l'authentification et rajout d'un bouton pour afficher password

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
+import {NgxSpinnerModule, NgxSpinnerService} from "ngx-spinner";
 
 @Component({
   selector: 'app-login',
@@ -9,7 +10,8 @@ import { CommonModule } from '@angular/common';
   imports: [
     FormsModule ,
     ReactiveFormsModule ,
-    CommonModule
+    CommonModule,
+    NgxSpinnerModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
@@ -18,8 +20,13 @@ export class LoginComponent implements OnInit
 {
   mailDefault: string = 'administrateur@gmail.com';
   passwordDefault: string = '0000';
+  showPassword = false;
 
-  constructor( private form_builder: FormBuilder , private auth_service: AuthService ) {}
+  constructor(
+    private form_builder: FormBuilder ,
+    private auth_service: AuthService,
+    private spinner: NgxSpinnerService
+  ) {}
 
   login_form: FormGroup ;
 
@@ -28,12 +35,19 @@ export class LoginComponent implements OnInit
     this.login_form = this.form_builder.group( {
       email: [ this.mailDefault , [ Validators.required , Validators.email ] ] ,
       password: [ this.passwordDefault , Validators.required ]
-    } ) 
+    } )
   }
 
   login()
   {
-    this.auth_service.login( this.login_form.value.email , this.login_form.value.password ) ;
+    this.spinner.show() ;
+    setTimeout( () => {
+      this.auth_service.login( this.login_form.value.email , this.login_form.value.password ) ;
+    }, 2000 ) ;
+  }
+
+  EnableShowPassword() {
+    this.showPassword = !this.showPassword;
   }
 
 }
